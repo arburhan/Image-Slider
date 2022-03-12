e81cbd73cb6cf49ea30ec3e55ff4ba10b2886e43
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-03-12 19:18:04 +0600

    setting interval

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const imagesID = document.getElementById('images');
+const imgArray = [1, 3, 5,3, 23, 53,32];
+let imgIndexNumber = 0;
 setInterval(() => {
-    console.log(`slider-`)
+    imgIndexNumber++;
+    imagesID.setAttribute('src', imgArray[imgIndexNumber]);
+    if(imgIndexNumber >= imgArray.length){
+        imgIndexNumber = 0;
+    }
+    console.log(imgIndexNumber);
 }, 2000);
